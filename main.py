d4f9d45ee9a2a7503152ed4cb4c454cd600f9beb
MariaEduardaLucena <hey.santxz@gmail.com>
2026-08-11 18:18:49 -0300

    semana16 aula 2

---
+++ main.py
@@ -0,0 +1,9 @@
+notas = [[7,8,6],[5,9,7],[8,6,10]]
+
+print (notas[1][1])
+
+for i in range (len(notas)):
+    print (notas[i])
+for i in range (len(notas)):
+    for j in range (len(notas)):
+        print (notas[i][j])
